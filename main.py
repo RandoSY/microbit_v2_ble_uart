1afddf83c6fa7bc869e9028a3287b4ed43ade858
RandoSY <RandoSY@users.noreply.github.com>
2024-08-25 19:15:01 -0400

    Update main.blocks, main.py

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-import bluetooth
+// Start the UART service (this will make the Micro:Bit advertise itself over BLE)
+bluetooth.startUartService()
 
-# Create a GATT service and characteristic for the UART
-service_uuid = bluetooth.UUID("12345678-1234-1234-1234-123456789012")
-char_uuid = bluetooth.UUID("12345678-1234-1234-1234-123456789013")
-service = bluetooth.GATTService(service_uuid, [bluetooth.GATTCharacteristic(char_uuid, bluetooth.GATT_READ | bluetooth.GATT_WRITE)])
+// Handle received data from the UART service
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    let received = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    bluetooth.uartWriteString(received)  // Echo the received message back
+})
 
-# Start advertising
-bluetooth.advertise(service_uuid, name="BLE UART Echo")
+// Optional: Add a visual indication that the Micro:Bit is ready and advertising
+basic.showString("ADV")
 
-# Function to handle incoming data
-def handle_read(client_id, attribute, data):
-    # Echo the received data back to the client
-    bluetooth.gatt_server.send(client_id, attribute, data)
-
-# Set the callback for incoming data
-bluetooth.gatt_server.register_callback(handle_read)
-
-# Start the GATT server
-bluetooth.gatt_server.start()
-
-# Main loop
-while True:
-    pass
+basic.forever(function () {
+    basic.pause(100)  // Keep the program running
+})
 
